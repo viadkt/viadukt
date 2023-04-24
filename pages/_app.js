980b6456bc2c84a58f2eea4ebace718f76104a50
j0hannr <johann.rohn@gmail.com>
2023-04-24 23:36:31 +0200

    👻 SEO & LogRocket

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,27 @@
-import '@/styles/globals.css'
-import { Analytics } from '@vercel/analytics/react';
+import "@/styles/globals.css";
+import { Analytics } from "@vercel/analytics/react";
+import { DefaultSeo } from "next-seo";
+import LogRocket from "logrocket";
+
+// Init LogRocket only in Production
+if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production") LogRocket.init('vjissv/viadukt');
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <DefaultSeo
+        description=""
+        openGraph={{
+          type: "website",
+          locale: "de_DE",
+          url: "https://viadukt.de",
+          site_name: "Viadukt",
+          title: "Viadukt",
+          description: "Viadukt — Demokratische Stadtentwicklung",
+        }}
+      />
       <Component {...pageProps} />
       <Analytics />
     </>
-  )
+  );
 }
